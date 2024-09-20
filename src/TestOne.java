655d352bbf8d85b929c75886267be4d1a9fef8a7
VJAINBH <bhuvnesh.jain@pearson.com>
2024-09-20 14:26:23 +0530

    changes stream api

--- src/TestOne.java
+++ src/TestOne.java
@@ -4,6 +4,6 @@ import java.util.stream.Stream;
 public class TestOne {
     public static void main(String[] args) {
         Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        integerStream.forEach(integer -> System.out.println("printing number : " + integer));
+        integerStream.forEach(integer -> System.out.println("printing number using stream api : " + integer));
     }
 }
